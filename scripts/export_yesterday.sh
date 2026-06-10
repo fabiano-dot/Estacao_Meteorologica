2755194addad0d1cf4a5cbcec54181c93bf9f401
adr1t0s <adritosmh@gmail.com>
2026-06-10 00:50:07 -0300

    Migracao hugging faces

--- scripts/export_yesterday.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-ROOT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
-cd "$ROOT_DIR"
-
-DATE=$(TZ="America/Sao_Paulo" date -d "yesterday" +%Y-%m-%d)
-
-bash "$ROOT_DIR/scripts/export_for_date.sh" "$DATE"
